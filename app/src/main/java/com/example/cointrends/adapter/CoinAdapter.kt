9f10a36fd7ebfac186bc96e93579004702699a08
jakins177 <jakins177@email.com>
2021-09-27 23:01:33 -0400

    implemented ui for trending coins list

--- app/src/main/java/com/example/cointrends/adapter/CoinAdapter.kt
+++ app/src/main/java/com/example/cointrends/adapter/CoinAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.cointrends.adapter
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cointrends.R
 import com.example.cointrends.model.CoinX
+import com.olayg.onlykats.util.loadWithGlide
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class CoinAdapter(private val dataSet:
                   List<CoinX>? = mutableListOf()) :
@@ -32,12 +37,32 @@ class CoinAdapter(private val dataSet:
 
 
     inner class TrendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private var coinName : TextView = itemView.findViewById(R.id.coin_info_text_view)
+        private var coinName : TextView = itemView.findViewById(R.id.coinNameTextView)
+        private var coinImage : ImageView = itemView.findViewById(R.id.coinImageView)
+        private var coinPrice : TextView = itemView.findViewById(R.id.priceTextView)
+        private var coinShortHand : TextView = itemView.findViewById(R.id.shortHandTextView)
+
 
         fun updateInfo (item: CoinX?) {
             if (item != null && item.item != null) {
 
                 coinName.text = item.item.name
+
+                item.item.thumb?.let { coinImage.loadWithGlide(it) }
+
+
+
+                coinPrice.text = item.item.priceBtc.toString()
+
+                coinShortHand.text = item.item.symbol
+
+              var marketPrice = item.item.priceBtc?.toBigDecimal()
+
+
+                coinPrice.text = marketPrice?.setScale(6,RoundingMode.HALF_EVEN).toString()
+
+
+
             };
         }
     }
